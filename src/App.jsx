df954fe6a751a0b77a40815b6a2e14f788f9e941
KanishkSogani <kanishksogani1@gmail.com>
2024-04-13 12:24:35 +0530

    Displaying new friend form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,12 +22,20 @@ const initialFriends = [
 ];
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <AddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <AddFriend />}
+        <Button onClick={handleAddFriend}>
+          {!showAddFriend ? "Add friend" : "Close"}
+        </Button>
       </div>
 
       <SpiltBill />
@@ -65,8 +75,12 @@ function Friend({ friend }) {
   );
 }
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 function AddFriend() {
